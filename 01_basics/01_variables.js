0b921ff71b5566c4170eb8d3aeceb69627c4212e
HRS2511 <140520913+HRS2511@users.noreply.github.com>
2023-07-25 18:28:35 +0000

    lecture 1 variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 34563456
+let accountEmail = "harsh@123.com"
+var accountPassword = "12345"
+acountCity = "Jaipur"
+let accountState;
+
+// ----------
+
+// accountEmail = "hrs123@gmail.com"
+// accountPassword = "3232"
+// acountCity = "bengaluru"
+
+/*
+prefer not to use var
+because of issue in block scopr and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, acountCity, accountState])
+
